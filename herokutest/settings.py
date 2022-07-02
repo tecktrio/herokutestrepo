5454db64dade1f30f56762d4a6bb8523af3ae354
tecktrio <amalpullan4@gmail.com>
2022-07-02 15:51:14 +0530

    w

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'herokutest/static')
+    os.path.join(BASE_DIR,'herokutestapp/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
