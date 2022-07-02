457b03e7cbef11daafd70e1e335c82ac00070a88
tecktrio <amalpullan4@gmail.com>
2022-07-02 12:10:08 +0530

    w

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['tecktrio.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'herokutestapp.apps.HerokutestappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
