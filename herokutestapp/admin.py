7496400d5bc1be31f47a7c0606c22ad04c050665
tecktrio <amalpullan4@gmail.com>
2022-07-02 19:00:43 +0530

    w

--- herokutestapp/admin.py
+++ herokutestapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from herokutestapp.models import ProductList
+
+admin.site.register(ProductList)
--- herokutestapp/models.py
+++ herokutestapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ProductList(models.Model):
+    pro_name = models.TextField()
+    pro_description = models.TextField()
+    pro_rate = models.IntegerField()
+    pro_img = models.ImageField()
